945be5fdcb9cea3f002fd90d67458ee3776d9b66
Hideto Maguchi <hidetomaguchi@gmail.com>
2023-05-14 20:55:26 +0900

    設定画面を追加

---
+++ Growth/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  Growth
+//
+//  Created by 間口秀人 on 2023/05/14.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
